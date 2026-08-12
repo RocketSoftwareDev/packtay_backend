2e96d1933963ef479e55fe2a7911a9e549928fbe
Christopher Vera <85882973+Chriss78Vera@users.noreply.github.com>
2026-08-11 19:45:00 -0500

    fix: expose bearer authorization in Swagger

--- business-svc/src/main/java/ec/paktay/business/config/OpenApiConfig.java
+++ business-svc/src/main/java/ec/paktay/business/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package ec.paktay.business.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ public class OpenApiConfig {
         return new OpenAPI().info(new Info().title("Paktay Business API").version("v1")
                 .description("Lógica de negocio financiera. Todas las operaciones de datos requieren un JWT de Keycloak."))
                 .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                        .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
